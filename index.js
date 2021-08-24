fdab4f4d42e26ca6a31c3692b9c7708b1c2c6cec
Shawna Akiona <shawna.akiona@gmail.com>
2021-08-24 14:19:43 -0700

    added button

--- index.js
+++ index.js
@@ -9,14 +9,19 @@ taskForm.addEventListener('submit', (e) => {
   const toDoList = document.createElement('ul')
 
   const newToDo = document.createElement('li')
-  newToDo.textContent = document.querySelector('#new-input').value
+  newToDo.textContent = document.querySelector('#new-input ').value
   
   const toDoButton = document.createElement('button')
+  toDoButton.addEventListener('click', handleDelete)
   toDoButton.textContent = "X"
 
   toDoList.append(newToDo, toDoButton)
   toDoContainer.append(toDoList)
 })
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
 
 // form.addEventListener("submit", (e) => {
 //   e.preventDefault();
